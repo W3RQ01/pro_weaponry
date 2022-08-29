be2eb60a9f6fc7da35c63b9d7c095a65287d6a6e
W3RQ01 <77688979+W3RQ01@users.noreply.github.com>
2022-08-29 19:20:02 +0200

    Added battlehammers
    added diamond, mese, iron and stone battlehammer

--- init.lua
+++ init.lua
@@ -182,3 +182,123 @@ minetest.register_craft({
                 {'','pro_weaponry:handle',''},
 	}
 })	
+
+
+minetest.register_tool("pro_weaponry:saber_stone", {
+	description = "Stone Saber",
+	inventory_image = "saber_stone.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=90, maxlevel=3},
+		},
+		damage_groups = {fleshy=10},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:saber_stone",
+        recipe = {
+                {'','default:cobble','default:cobble'},
+                {'','default:cobble',''},
+                {'','pro_weaponry:handle',''},
+	}
+})
+
+
+minetest.register_tool("pro_weaponry:hammer_diamond", {
+	description = "Diamond Battlehammer",
+	inventory_image = "hammer_diamond.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=150, maxlevel=3},
+		},
+		damage_groups = {fleshy=17},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:hammer_diamond",
+        recipe = {
+                {'default:diamond','default:diamond','default:diamond'},
+                {'default:diamond','default:diamond','default:diamond'},
+                {'','pro_weaponry:handle',''},
+	}
+})	
+
+
+minetest.register_tool("pro_weaponry:hammer_mese", {
+	description = "Mese Battlehammer",
+	inventory_image = "hammer_mese.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=120, maxlevel=3},
+		},
+		damage_groups = {fleshy=15},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:hammer_diamond",
+        recipe = {
+                {'default:mese_crystal','default:mese_crystal','default:mese_crystal'},
+                {'default:mese_crystal','default:mese_crystal','default:mese_crystal'},
+                {'','pro_weaponry:handle',''},
+	}
+})	
+
+
+minetest.register_tool("pro_weaponry:hammer_iron", {
+	description = "Iron Battlehammer",
+	inventory_image = "hammer_iron.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=105, maxlevel=3},
+		},
+		damage_groups = {fleshy=13},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:hammer_diamond",
+        recipe = {
+                {'default:iron_ingot','default:iron_ingot','default:iron_ingot'},
+                {'default:iron_ingot','default:iron_ingot','default:iron_ingot'},
+                {'','pro_weaponry:handle',''},
+	}
+})	
+
+
+minetest.register_tool("pro_weaponry:hammer_stone", {
+	description = "Stone Battlehammer",
+	inventory_image = "hammer_stone.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=90, maxlevel=3},
+		},
+		damage_groups = {fleshy=10},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:hammer_diamond",
+        recipe = {
+                {'default:cobble','default:cobble','default:cobble'},
+                {'default:cobble','default:cobble','default:cobble'},
+                {'','pro_weaponry:handle',''},
+	}
+})	
