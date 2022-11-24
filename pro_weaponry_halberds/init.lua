fef0632172d47327881f488e1fe131be4d8cc5f4
W3RQ01 <77688979+W3RQ01@users.noreply.github.com>
2022-11-24 18:49:14 +0100

    Update init.lua

--- pro_weaponry_halberds/init.lua
+++ pro_weaponry_halberds/init.lua
@@ -1,1 +1,49 @@
- 
+-- Author: W3RQ01
+-- License: GPL 3.0
+
+minetest.register_tool("pro_weaponry:halberd_diamond", {
+	description = "Diamond Halberd",
+	inventory_image = "halberd_diamond.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1.7,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=150, maxlevel=3},
+		},
+		damage_groups = {fleshy=20.5},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:halberd_diamond",
+        recipe = {
+                {'','','default:diamond'},
+                {'default:diamond','default:diamond',''},
+                {'default:diamond','pro_weaponry:pole',''},
+	}
+})	
+
+
+minetest.register_tool("pro_weaponry:halberd_mese", {
+	description = "Mese Halberd",
+	inventory_image = "halberd_mese.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1.7,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=150, maxlevel=3},
+		},
+		damage_groups = {fleshy=17},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:halberd_mese",
+        recipe = {
+                {'','','default:mese_crystal'},
+                {'default:mese_crystal','default:mese_crystal',''},
+                {'default:mese_crystal','pro_weaponry:pole',''},
+	}
+})
