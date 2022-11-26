a1ecc9749cad5628132a0b0bc3443aadd83c28a1
W3RQ01 <77688979+W3RQ01@users.noreply.github.com>
2022-11-26 19:14:39 +0100

    added iron and stone halberd

--- pro_weaponry_halberds/init.lua
+++ pro_weaponry_halberds/init.lua
@@ -20,7 +20,7 @@ minetest.register_craft({
         recipe = {
                 {'','','default:diamond'},
                 {'default:diamond','default:diamond',''},
-                {'default:diamond','pro_weaponry:pole',''},
+                {'default:diamond','pro_weaponry_tools:pole',''},
 	}
 })	
 
@@ -44,6 +44,54 @@ minetest.register_craft({
         recipe = {
                 {'','','default:mese_crystal'},
                 {'default:mese_crystal','default:mese_crystal',''},
-                {'default:mese_crystal','pro_weaponry:pole',''},
+                {'default:mese_crystal','pro_weaponry_tools:pole',''},
+	}
+})
+
+
+minetest.register_tool("pro_weaponry:halberd_iron", {
+	description = "Iron Halberd",
+	inventory_image = "halberd_iron.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1.7,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=150, maxlevel=3},
+		},
+		damage_groups = {fleshy=14},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:halberd_iron",
+        recipe = {
+                {'','','default:iron_ingot'},
+                {'default:iron_ingot','default:iron_ingot',''},
+                {'default:iron_ingot','pro_weaponry_tools:pole',''},
+	}
+})
+
+
+minetest.register_tool("pro_weaponry:halberd_stone", {
+	description = "Stone Halberd",
+	inventory_image = "halberd_stone.png",
+	wield_scale = {x=2, y=3, z=1},
+	tool_capabilities = {
+		full_punch_interval = 1.7,
+		max_drop_level=3,
+		groupcaps={
+			oddly_breakable_by_hand={times={[1]=2.00, [2]=1.20, [3]=0.20}, uses=150, maxlevel=3},
+		},
+		damage_groups = {fleshy=10},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+minetest.register_craft({
+        output = "pro_weaponry:halberd_stone",
+        recipe = {
+                {'','','default:cobble'},
+                {'default:cobble','default:cobble',''},
+                {'default:cobble','pro_weaponry_tools:pole',''},
 	}
 })
